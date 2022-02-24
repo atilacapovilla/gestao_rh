e06adab3c9b7aca60b5dd0ddc481de54c5381520
Atila Capovilla <capovillaatila@gmail.com>
2022-02-24 16:31:09 -0300

    Implementação CRUD de Empresas

--- apps/empresas/views.py
+++ apps/empresas/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Empresa
 
-# Create your views here.
+class EmpresaCreate(CreateView):
+    model = Empresa
+    fields = ['nome']
+
+    # sobescrevendo o metodo form_valid 
+    # para atualizar a empresa do funcionario
+    def form_valid(self, form):
+        obj = form.save()
+        funcionario = self.request.user.funcionario
+        funcionario.empresa = obj
+        funcionario.save()
+        return HttpResponse('ok')
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
+    
