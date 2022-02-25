491656f99b659c29cd3f968248ee1ee3e459f961
Atila Capovilla <capovillaatila@gmail.com>
2022-02-25 17:29:54 -0300

    Implementação CRUD de Funcionarios

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
 
@@ -10,5 +11,9 @@ class Funcionario(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
 
 
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
+
+    
     def __str__(self):
         return self.nome
