b98e939dc1ee3af2ece3b0b5ee9fc18a261ec68a
atilacapovilla <capovillaatila@gmail.com>
2024-09-12 16:42:22 -0300

    Criando os relacionamentos de banco de dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from apps.funcionarios.models import Funcionario
+
 
 class Documento(models.Model):
     descricao = models.CharField(
-        max_length=100, help_text='Desrição do Documento')
+        max_length=100, help_text='Descrição do Documento')
+    pertence = models.ForeignKey(
+        Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from apps.funcionarios.models import Funcionario
+
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=50, help_text='Motivo da hora extra')
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
